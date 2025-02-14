988c1f3631c0e42644fd9ead016b0f1db0b3fbcc
devdutt07 <thawaredevdutt1@gmail.com>
2025-02-14 19:15:13 +0530

    feat: Ignore numbers greater than 1000 in sum calculation

--- add.js
+++ add.js
@@ -1,35 +1,34 @@
 function add(numStr) {
-    //  empty str check
+    // Empty string check
     if (numStr === '') {
         return 0;
-      }
-    //   return number itself
+    }
+
+    // Return number itself
     if (!numStr.includes(',')) {
         return parseInt(numStr, 10);
-      }
+    }
 
-      // delimiter checks  
-      let delimiter = /[\n,]/;
-      if (numStr.startsWith('//')) {
-          const match = numStr.match(/^\/\/(.+)\n(.*)/);
-          if (match) {
-              delimiter = new RegExp(match[1]);
-              numStr = match[2];
-          }
-      }
+    // Delimiter checks  
+    let delimiter = /[\n,]/;
+    if (numStr.startsWith('//')) {
+        const match = numStr.match(/^\/\/(.+)\n(.*)/);
+        if (match) {
+            delimiter = new RegExp(match[1]);
+            numStr = match[2];
+        }
+    }
 
-    
-      const numStrArray = numStr.split(delimiter).map(Number);
-      const negatives = numStrArray.filter(num => num < 0);
-  
-    //   step 5 negative check
-      if (negatives.length > 0) {
-          throw new Error(`Negatives not allowed: ${negatives.join(', ')}`);
-      }
-  
-      return numStrArray.reduce((sum, val) => sum + val, 0); 
-       
+    const numStrArray = numStr.split(delimiter).map(Number);
+    const negatives = numStrArray.filter(num => num < 0);
+
+    // Step 5: Negative number check
+    if (negatives.length > 0) {
+        throw new Error(`Negatives not allowed: ${negatives.join(', ')}`);
+    }
+
+    // Step 7: Ignore numbers greater than 1000
+    return numStrArray.filter(num => num <= 1000).reduce((sum, val) => sum + val, 0);
 }
 
-
-module.exports = add
+module.exports = add;
