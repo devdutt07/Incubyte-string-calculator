67850245b4e5e2ac2f1313dd2aafbd15c0fca3a8
devdutt07 <thawaredevdutt1@gmail.com>
2025-02-14 18:51:21 +0530

    fest: added function & test case for single input

--- add.js
+++ add.js
@@ -1,7 +1,13 @@
 function add(numStr) {
+    //  empty str check
     if (numStr === '') {
         return 0;
       }
+    //   return number itself
+    if (!numStr.includes(',')) {
+        return parseInt(numStr, 10);
+      }
+
 }
 
 
